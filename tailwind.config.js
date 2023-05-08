26ae649eea9eeef05ae0622992cb79b466ca26b0
TataneSan <65590382+TataneSan@users.noreply.github.com>
2023-05-08 17:34:43 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+      extend: {
+        colors: {
+            primary: "#1f2122",
+            secondary: "#403255",
+            third: "#e75e8d",
+        }
+    },
+  },
+  plugins: [],
+}
